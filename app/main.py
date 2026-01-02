44fe8128b2886388993c6062375c5f9822d24f1e
bhargavsai259 <bhargavsaikumar259@gmail.com>
2026-01-03 01:33:36 +0530

    all routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.db.base import engine
+from app.db import model
+from app.api.tasks import router as task_router
+
+model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="ChatTask Manager")
+
+app.include_router(task_router)
 
 @app.get("/")
 def health_check():
